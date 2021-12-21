271c32508e74eb30b2b62b6da901bc447550c9dd
iknow <nonsense.com>
2021-12-20 19:38:01 -0500

    initial creation

---
+++ Sales2018/conm/fletch/Main.py
@@ -0,0 +1,16 @@
+'''
+Created on Dec 20, 2021
+
+@author: iknow
+'''
+
+if __name__ == '__main__':
+    print("initialput1 1");
+    print("init9alput1 2");
+    print("initialput 3 ")
+    print ("initial put 5")
+    print ("initial put 6")
+    print ("initial put 7")
+    print ("initial put 8")
+    print ("initial put 9")
+    pass

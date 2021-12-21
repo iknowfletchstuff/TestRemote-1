bb2af9e770da6a2ea0ad679efc794b666ce80aad
iknow <nonsense.com>
2021-12-20 19:53:45 -0500

    CHand  line 8, and line 11

--- Sales2018/conm/fletch/Main.py
+++ Sales2018/conm/fletch/Main.py
@@ -5,10 +5,10 @@ Created on Dec 20, 2021
 '''
 
 if __name__ == '__main__':
-    print("initialput1 1");
+    print("ChangeDefulat Branch changes these values inital input 1");
     print("init9alput1 2");
     print("initialput 3 ")
-    print ("initial put 5")
+    print ("Create a nwe for loop here initial put 5")
     print ("initial put 6")
     print ("initial put 7")
     print ("initial put 8")
